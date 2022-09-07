a2d52f736b7377ba10710623754de9400887241d
Beatriz Dantas <biatriz.helena.21@gmail.com>
2022-09-07 19:01:47 -0300

    Ajustes do front

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Meu Carrinho</h1>
+        <h1>Meu carrinho</h1>
         <hr></hr>
       </header>
       <div className='container'>
         <ul>
           <Cart />
+          <Cart />
         </ul>
       </div>
+      <footer>
+        
+        <hr></hr>
+        <div>
+          <h2>Total</h2>
+          <spam>R$ 9,55</spam>
+        </div>
+        <p>Parabéns, sua compra tem frete grátis! (colocar em verde)</p>
+        <hr></hr>
+        <button> Finalizar compra</button>
+      
+      </footer>
     </div>
   );
 }
--- src/components/cart.js
+++ src/components/cart.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './cart.css';
 
 export default function Cart() {
   return (
     <li className='item'>
-      <img></img>
-      <span>30,00</span>
-      <h2>Trunfa</h2>
+      <img src="http://codeby.vteximg.com.br/arquivos/ids/159939-800-1029/trufa-morango-30g.png?v=636916431597070000"></img>
+      <div className='words'>
+        <h2>Trufa de Morango</h2>
+        <span>R$ 1,23</span>
+      </div>
     </li>
   );
 }
