67b6b44d9b62c35e0f9f605096d07792f38b9729
Beatriz Dantas <biatriz.helena.21@gmail.com>
2022-09-07 18:12:11 -0300

    estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Cart from './components/cart.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meu Carrinho</h1>
+        <hr></hr>
       </header>
+      <div className='container'>
+        <ul>
+          <Cart />
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Cart() {
+  return (
+    <li className='item'>
+      <img></img>
+      <span>30,00</span>
+      <h2>Trunfa</h2>
+    </li>
+  );
+}
